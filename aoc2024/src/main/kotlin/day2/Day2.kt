ccb75a6fba58b7e9db7141f8bd2c1886bdf50627
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-02 07:23:42 +0100

    day 2

--- aoc2024/src/main/kotlin/day2/Day2.kt
+++ aoc2024/src/main/kotlin/day2/Day2.kt
@@ -3,12 +3,29 @@ package day2
 import readAllText
 
 fun main() {
-    println(part1(readAllText("local/day2_input.txt")))
-    println(part2(readAllText("local/day2_input.txt")))
+    val data = parse(readAllText("local/day2_input.txt"))
+    println(part1(data))
+    println(part2(data))
 }
 
-fun part1(input: String) = input.lineSequence().filterNot(String::isBlank)
-    .count()
+fun part1(input: List<List<Long>>) = input.count(Iterable<Long>::isSafe)
 
-fun part2(input: String) = input.lineSequence().filterNot(String::isBlank)
-    .count()
+fun part2(input: List<List<Long>>) = input.count { line ->
+    line.indices.asSequence()
+        .map(line::skippingOne)
+        .any(Iterable<Long>::isSafe)
+}
+
+private fun List<Long>.skippingOne(indexToSkip: Int): Iterable<Long> = Iterable<Long> {
+    object : Iterator<Long> {
+        var i = if (indexToSkip == 0) 1 else 0
+        override fun next(): Long = this@skippingOne[i++].also { if (i == indexToSkip) i++ }
+        override fun hasNext(): Boolean = i < this@skippingOne.size
+    }
+}
+
+fun Iterable<Long>.isSafe(): Boolean = zipWithNext().all { (a, b) -> (b - a) in (1..3) } ||
+        zipWithNext().all { (a, b) -> (a - b) in (1..3) }
+
+fun parse(input: String): List<List<Long>> = input.lineSequence().filterNot(String::isBlank)
+    .map { it.split(" ").map { it.toLong() } }.toList()

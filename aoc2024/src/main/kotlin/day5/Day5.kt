1cf0dfac5ff9887743e4db81437957bbb1a95f25
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-05 07:56:34 +0100

    day 5 correct

--- aoc2024/src/main/kotlin/day5/Day5.kt
+++ aoc2024/src/main/kotlin/day5/Day5.kt
@@ -10,52 +10,40 @@ fun main() {
 }
 
 data class Input(
-    val ordering: List<List<String>>,
+    val ordering: List<Pair<String, String>>,
     val updates: List<List<String>>,
 )
 
-// 5248
-fun part1(input: Input) = input.let { (ordering, updates) ->
-    val afters = afters(ordering)
-    updates.filter { isCorrect(it, afters) }
-        .sumOf { it[it.size / 2].toLong() }
+fun part1(input: Input): Long {
+    val comparator = Day5Comparator(input.ordering)
+    val correct = input.updates
+        .filter { it == it.sortedWith(comparator) }
+    return correct.sumOf { it[it.size / 2].toLong() }
 }
 
-// 4507
-fun part2(input: Input) = input.let { (ordering, updates) ->
-    val afters = afters(ordering)
-    updates.filterNot { isCorrect(it, afters) }
-        .map { it.sortedWith(Day5Comparator(afters)) }
-        .sumOf { it[it.size / 2].toLong() }
+fun part2(input: Input): Long {
+    val comparator = Day5Comparator(input.ordering)
+    val incorrectSorted = input.updates
+        .mapNotNull { it.sortedWith(comparator).takeIf { sorted -> sorted != it } }
+    return incorrectSorted.sumOf { it[it.size / 2].toLong() }
 }
 
-private fun parse(input: String) = input.lines().let { lines ->
-    val s = lines.indexOfFirst(String::isBlank)
-    Input(
-        lines.take(s).map { it.split("|") },
-        lines.drop(s+1).dropLastWhile(String::isBlank).map { it.split(",") },
-    )
-}
+class Day5Comparator(private val afters: Map<String, Set<String>>) : Comparator<String> {
+    constructor(ordering: List<Pair<String, String>>) : this(buildMap<String, MutableSet<String>> {
+        ordering.forEach { (x, y) -> getOrPut(x) { mutableSetOf() }.add(y) }
+    })
 
-private fun afters(ordering: List<List<String>>): Map<String, Set<String>> {
-    val afters = mutableMapOf<String, MutableSet<String>>()
-    ordering.forEach { (x, y) -> afters.getOrPut(x) { mutableSetOf() }.add(y) }
-    return afters
-}
-
-private fun isCorrect(
-    update: List<String>,
-    afters: Map<String, Set<String>>
-) = update.indices.all { i ->
-    val s = update[i]
-    val a = afters[s].orEmpty()
-    update.take(i).none { x -> x in a } && update.drop(i + 1).none { y -> s in afters[y].orEmpty() }
-}
-
-class Day5Comparator(val afters: Map<String, Set<String>>):Comparator<String> {
     override fun compare(x: String?, y: String?): Int = when {
         y in afters[x].orEmpty() -> -1
         x in afters[y].orEmpty() -> 1
         else -> 0
     }
+}
+
+private fun parse(input: String) = input.lines().let { lines ->
+    val s = lines.indexOfFirst(String::isBlank)
+    Input(
+        lines.take(s).map { it.split("|").let { (a, b) -> a to b } },
+        lines.drop(s + 1).dropLastWhile(String::isBlank).map { it.split(",") },
+    )
 }

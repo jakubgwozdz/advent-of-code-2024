b5e250e8ede168c53241f6b11eee56f712dea439
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-06 07:08:29 +0100

    day 5 notebook tries, day 6

--- aoc2024/src/main/kotlin/day4/Day4.kt
+++ aoc2024/src/main/kotlin/day4/Day4.kt
@@ -1,6 +1,7 @@
 package day4
 
 import readAllText
+import grid.*
 
 fun main() {
     val input = readAllText("local/day4_input.txt").lines()
@@ -19,20 +20,3 @@ fun part2(grid: Grid): Int = grid.findAll('A').count { p ->
     diagonals.count { m -> grid[p - m] == 'M' && grid[p + m] == 'S' } == 2
 }
 
-typealias Move = Pair<Int, Int>
-
-val diagonals: Set<Move> = setOf(-1 to -1, -1 to 1, 1 to -1, 1 to 1)
-val vertical: Set<Move> = setOf(-1 to 0, 1 to 0)
-val horizontal: Set<Move> = setOf(0 to -1, 0 to 1)
-
-typealias Pos = Pair<Int, Int>
-
-operator fun Pos.plus(d: Move) = Pos(first + d.first, second + d.second)
-operator fun Pos.minus(d: Move) = Pos(first - d.first, second - d.second)
-
-typealias Grid = List<String>
-
-operator fun Grid.get(p: Pos) = getOrNull(p.first)?.getOrNull(p.second)
-fun Grid.findAll(ch: Char): Sequence<Pos> = asSequence().flatMapIndexed { r, line ->
-    line.indices.filter { line[it] == ch }.map { c -> Pos(r, c) }
-}
--- aoc2024/src/main/kotlin/day6/Day6.kt
+++ aoc2024/src/main/kotlin/day6/Day6.kt
@@ -1,14 +1,42 @@
 package day6
 
 import readAllText
+import grid.*
 
 fun main() {
-    println(part1(readAllText("local/day6_input.txt")))
-    println(part2(readAllText("local/day6_input.txt")))
+    val input = readAllText("local/day6_input.txt").lines()
+    println(part1(input).also { check(it == 4819) })
+    println(part2(input).also { check(it == 1796) })
 }
 
-fun part1(input: String) = input.lineSequence().filterNot(String::isBlank)
-    .count()
+fun part1(grid: Grid): Int = visited(grid).size
 
-fun part2(input: String) = input.lineSequence().filterNot(String::isBlank)
-    .count()
+fun part2(grid: Grid): Int = visited(grid).filter { grid[it] == '.' }.count { extra ->
+    val start = grid.findAll('^').single()
+    var guard = start to Dir.U
+    val visited = mutableSetOf<Pair<Pos, Dir>>()
+    while (guard.first in grid) {
+        visited += guard
+        guard = step(guard) { grid[it] != '#' && it != extra }
+        if (guard in visited) return@count true
+    }
+    false
+}
+
+fun step(guard: Pair<Pos, Dir>, availableOp: (Pos) -> Boolean): Pair<Pos, Dir> {
+    val (pos, dir) = guard
+    val forward = pos + dir
+    return if (availableOp(forward)) forward to dir
+    else pos to dir.turnRight()
+}
+
+private fun visited(grid: Grid): Set<Pos> {
+    val start = grid.findAll('^').single()
+    var guard = start to Dir.U
+    return buildSet {
+        while (guard.first in grid) {
+            add(guard.first)
+            guard = step(guard) { grid[it] != '#' }
+        }
+    }
+}

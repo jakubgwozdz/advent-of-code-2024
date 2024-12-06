c13b47b85b14e4b8c2a97657b0452c492435b998
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-06 21:58:43 +0100

    day 6 cached

--- aoc2024/src/main/kotlin/day6/Day6.kt
+++ aoc2024/src/main/kotlin/day6/Day6.kt
@@ -11,33 +11,52 @@ fun main() {
 
 fun part1(grid: Grid): Int {
     val start = grid.indexOf('^')
-    return buildSet { patrol(grid, start) { add(it) } }.size
+    val cache = mutableMapOf<Pair<Dir, Pos>, Pos>()
+    return buildSet { patrol(grid, start, cache) { add(it) } }.size
 }
 
 fun part2(grid: Grid): Int {
     val start = grid.indexOf('^')
-    val valid = buildSet { patrol(grid, start) { if (it != start) add(it) } }
-    return valid.count { extra -> patrol(grid, start, extra) }
+    val cache = mutableMapOf<Pair<Dir, Pos>, Pos>()
+    val valid = buildSet { patrol(grid, start, cache) { if (it != start) add(it) } }
+    return valid.count { extra -> patrol(grid, start, cache, extra) }
 }
 
 // returns true if cycle is found, false if exits the grid
-private fun patrol(grid: Grid, start: Pos, extra: Pos? = null, op: (Pos) -> Unit = {}): Boolean {
+private fun patrol(
+    grid: Grid, start: Pos,
+    cache: MutableMap<Pair<Dir, Pos>, Pos>,
+    extra: Pos? = null,
+    op: (Pos) -> Unit = {}
+): Boolean {
     var pos = start
     var dir = Dir.U
-    val turns = mutableSetOf<Pair<Pos, Pos>>()
+    val turns = mutableSetOf<Pair<Dir, Pos>>()
+    val temp = mutableSetOf<Pos>()
     while (true) {
-        op(pos)
-        val forward = pos + dir
-        if (forward !in grid) return false
-        if (grid[forward] != '#' && forward != extra) pos = forward
-        else {
-            val turn = pos to forward
-            if (turn in turns) return true
-            turns += turn
-            dir = dir.turnRight()
-        }
+        var prev: Pos
+        do {
+            prev = pos
+            op(pos)
+            temp += pos
+            val cached = cache[dir to pos]?.takeIf { extra != null && !commonLine(pos, extra) }
+            if (cached != null) {
+                prev = cached
+                pos = prev + dir
+            } else pos += dir
+        } while (pos in grid && grid[pos] != '#' && pos != extra)
+        if (pos !in grid) return false
+        pos = prev
+        if (extra == null) temp.forEach { cache[dir to it] = pos }
+        temp.clear()
+        val turn = dir to pos
+        if (dir == Dir.U && turn in turns) return true
+        if (dir == Dir.U) turns += turn
+        dir = dir.turnRight()
     }
 }
+
+private fun commonLine(pos: Pos, extra: Pos?) = pos.first == extra?.first || pos.second == extra?.second
 
 typealias Pos = Pair<Int, Int>
 typealias Grid = List<String>
@@ -45,9 +64,10 @@ typealias Grid = List<String>
 operator fun Grid.get(p: Pos) = getOrNull(p.first)?.getOrNull(p.second)
 operator fun Grid.contains(p: Pos) = p.first in indices && p.second in get(p.first).indices
 
-fun Grid.indexOf(ch: Char):Pos = indexOfFirst { ch in it }.let { r-> r to this[r].indexOf(ch) }
+fun Grid.indexOf(ch: Char): Pos = indexOfFirst { ch in it }.let { r -> r to this[r].indexOf(ch) }
 
 enum class Dir { U, R, D, L }
+
 fun Dir.turnRight() = Dir.entries[(ordinal + 1) % Dir.entries.size]
 operator fun Pos.plus(d: Dir) = when (d) {
     Dir.U -> first - 1 to second

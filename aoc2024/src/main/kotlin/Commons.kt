41aa162f8cfb9c90f4287a80f51da1d2164fdb9a
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-06 11:06:20 +0100

    day 6 no concurrency, just fast

--- aoc2024/src/main/kotlin/Commons.kt
+++ aoc2024/src/main/kotlin/Commons.kt
@@ -1,5 +1,14 @@
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.time.measureTime
 
 fun readAllText(filePath: String): String = Files.readString(Path.of(filePath))
 fun String.linesWithoutLastBlanks(): List<String> = lines().dropLastWhile(String::isEmpty)
+
+inline fun<T> go(expected: T? = null, op: ()->T) {
+    measureTime {
+        val result = op()
+        println(result)
+        if (expected!=null) check(result==expected) { "expected $expected"}
+    }.also { println("took $it") }
+}
--- aoc2024/src/main/kotlin/day6/Day6.kt
+++ aoc2024/src/main/kotlin/day6/Day6.kt
@@ -1,34 +1,23 @@
 package day6
 
+import go
 import readAllText
-import grid.*
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.count
-import kotlinx.coroutines.flow.flattenMerge
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.runBlocking
 
 fun main() {
     val input = readAllText("local/day6_input.txt").lines()
-    println(part1(input).also { check(it == 4819) })
-    println(part2(input).also { check(it == 1796) })
+    go(expected = 4819) { part1(input) }
+    go(expected = 1796) { part2(input) }
 }
 
 fun part1(grid: Grid): Int {
-    val start = grid.findAll('^').single()
+    val start = grid.indexOf('^')
     return buildSet { patrol(grid, start) { add(it) } }.size
 }
 
 fun part2(grid: Grid): Int {
-    val start = grid.findAll('^').single()
-    return buildSet { patrol(grid, start) { if (it != start) add(it) } }
-        .run {
-            runBlocking {
-                map { extra -> flow { if (patrol(grid, start, extra)) emit(Unit) } }
-                    .asFlow().flattenMerge()
-                    .count()
-            }
-        }
+    val start = grid.indexOf('^')
+    val valid = buildSet { patrol(grid, start) { if (it != start) add(it) } }
+    return valid.count { extra -> patrol(grid, start, extra) }
 }
 
 // returns true if cycle is found, false if exits the grid
@@ -48,4 +37,21 @@ private fun patrol(grid: Grid, start: Pos, extra: Pos? = null, op: (Pos) -> Unit
             dir = dir.turnRight()
         }
     }
+}
+
+typealias Pos = Pair<Int, Int>
+typealias Grid = List<String>
+
+operator fun Grid.get(p: Pos) = getOrNull(p.first)?.getOrNull(p.second)
+operator fun Grid.contains(p: Pos) = p.first in indices && p.second in get(p.first).indices
+
+fun Grid.indexOf(ch: Char):Pos = indexOfFirst { ch in it }.let { r-> r to this[r].indexOf(ch) }
+
+enum class Dir { U, R, D, L }
+fun Dir.turnRight() = Dir.entries[(ordinal + 1) % Dir.entries.size]
+operator fun Pos.plus(d: Dir) = when (d) {
+    Dir.U -> first - 1 to second
+    Dir.R -> first to second + 1
+    Dir.D -> first + 1 to second
+    Dir.L -> first to second - 1
 }

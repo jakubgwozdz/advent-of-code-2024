09052c9b8efcd709a48e259ec4a0d5f98fc46456
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-09 11:35:57 +0100

    day9 sorted list

--- aoc2024/src/main/kotlin/day9/Day9.kt
+++ aoc2024/src/main/kotlin/day9/Day9.kt
@@ -5,20 +5,23 @@ import measure
 import readAllText
 
 data class Input(
-    val files: List<Int>,
+    val chunks: List<Int>,
     val free: List<Int>,
 )
 
-data class File(val id: Int, val start: Int, val size: Int)
+data class Chunk(val id: Int, val start: Int, val size: Int) {
+    val nextFree get() = start + size
+    override fun toString() = "$id @ $start..<$nextFree"
+}
 
 fun part1(input: Input): Long {
     var s = 0
-    val files = input.files.mapIndexed { idx, size ->
-        File(idx, s, size)
+    val chunks = input.chunks.mapIndexed { idx, size ->
+        Chunk(idx, s, size)
             .also { s += size + input.free.getOrElse(idx) { 0 } }
     }
     val memory = IntArray(s) { -1 }
-    files.forEach { (id, start, size) -> repeat(size) { memory[start + it] = id } }
+    chunks.forEach { (id, start, size) -> repeat(size) { memory[start + it] = id } }
     var end = memory.lastIndex
     var start = 0
     while (start < end) {
@@ -36,60 +39,43 @@ fun IntArray.checksum(): Long = foldIndexed(0L) { idx, acc, id ->
     acc + if (id > 0) id * idx else 0
 }
 
-private fun toStr(memory: IntArray) = memory
-    .joinToString(" ") { (if (it < 0) "." else "$it").padStart(4) }
-    .chunked(100)
-    .joinToString("\n")
-
 fun part2(input: Input): Long {
-    var i = 0
-    val files = input.files.withIndex().map { (id, length) ->
-        val range = i..<(i + length)
-        i += length
-        i += input.free.getOrElse(id) { 0 }
-        id to range
-    }.toMutableList()
-//    println(files)
-    val filesAsMap = files.toMap().toMutableMap()
-    input.files.indices.reversed().filter { it != 0 }.forEach { id ->
-        val original = filesAsMap[id]!!
-        val size = original.last - original.first + 1
-//        println("at $id, $original, $size")
-
-        // find free
-        var i = 1
-        var newRange: IntRange? = null
-        while (files[i - 1].second.last + 1 < original.first && newRange == null) {
-            newRange = (files[i - 1].second.last + 1)..(files[i].second.first - 1)
-            if (newRange.last - newRange.first + 1 < size) {
-                newRange = null
-                i++
-            }
-        }
-        if (newRange != null) {
-            newRange = newRange.first..<(newRange.first + size)
-            filesAsMap[id] = newRange
-            files.remove(id to original)
-            files.add(i, id to newRange)
+    var s = 0
+    val chunks = input.chunks.mapIndexed { idx, size ->
+        Chunk(idx, s, size)
+            .also { s += size + input.free.getOrElse(idx) { 0 } }
+    }
+    val compacted = SortedList(chunks, compareBy { it.start })
+    chunks.asReversed().forEach { original ->
+        val found: Int? = compacted.asSequence().zipWithNext()
+            .takeWhile { (f1, f2) -> f1.nextFree < original.start }
+            .firstOrNull { (f1, f2) -> f2.start - f1.nextFree >= original.size }
+            ?.first?.nextFree
+        if (found != null) {
+            compacted.remove(original)
+            compacted.add(original.copy(start = found))
         }
     }
-
-    val length = input.files.sum() + input.free.sum()
-    val memory = IntArray(length) { -1 }
-    files.forEach { (id, length) ->
-        length.forEach { i ->
-            if (memory[i] != -1) {
-                error("while setting $id memory[$i] already set to ${memory[i]}")
-            }
-            memory[i] = id
-        }
-    }
-    return memory.checksum()
-//    println(files)
-//    return files.sumOf { (id, range) -> range.sumOf { it * id }.toLong() }
+    return compacted.backing.sumOf { (id, start, size) -> (0..<size).sumOf { id.toLong() * (start + it) } }
 }
 
-// 5142018250274 wrong
+class SortedList<E>(initial: List<E>, val comparator: Comparator<E>) {
+    val backing: ArrayList<E> = ArrayList(initial)
+    fun asSequence(): Sequence<E> = backing.asSequence()
+    fun forEach(f: (E) -> Unit) = backing.forEach(f)
+    fun add(element: E) = backing.add(indexFor(element), element)
+    fun remove(element: E): Boolean = indexFor(element).let {
+        if (it in backing.indices) {
+            backing.removeAt(it)
+            true
+        } else false
+    }
+
+    // differs from indexOf
+    private fun indexFor(element: E): Int = backing.binarySearch(element, comparator).let {
+        if (it < 0) -it - 1 else it
+    }
+}
 
 fun parse(text: String): Input {
     val line = text.trim()
@@ -112,7 +98,6 @@ fun main() {
     go(6378826667552) { part1(input) }
     go(2858) { part2(test) }
     go(6413328569890) { part2(input) }
-    TODO()
     measure(text, parse = ::parse, part1 = ::part1, part2 = ::part2)
 }
 

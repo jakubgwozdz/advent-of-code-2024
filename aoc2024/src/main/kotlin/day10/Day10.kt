60c49fb943d752f8f994a43a9a3c21c67ffec01e
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-10 06:23:48 +0100

    day 10

--- aoc2024/src/main/kotlin/day10/Day10.kt
+++ aoc2024/src/main/kotlin/day10/Day10.kt
@@ -5,21 +5,62 @@ import linesWithoutLastBlanks
 import measure
 import readAllText
 
-typealias Input = List<String>
+typealias Input = Map<Int, Set<Pos>>
+typealias Pos = Pair<Int, Int>
 
-fun part1(input: Input) = input
-    .count()
+enum class Dir { U, R, D, L }
 
-fun part2(input: Input) = part1(input)
+operator fun Pos.plus(d: Dir) = when (d) {
+    Dir.U -> first - 1 to second
+    Dir.R -> first to second + 1
+    Dir.D -> first + 1 to second
+    Dir.L -> first to second - 1
+}
 
-fun parse(text: String) = text.linesWithoutLastBlanks()
+fun part1(input: Input): Int = input[0].orEmpty().sumOf { start->
+    var i = 0
+    var s = setOf(start)
+    while (i < 9) {
+        i++
+        val nexts = input[i].orEmpty()
+        s = Dir.entries.flatMap { d -> s.map { it + d }.filter { it in nexts } }.toSet()
+    }
+    s.count()
+}
+
+fun part2(input: Input) = input[0].orEmpty().sumOf { start->
+    var i = 0
+    var s = listOf(start)
+    while (i < 9) {
+        i++
+        s = Dir.entries.flatMap { d -> s.map { it + d }.filter { it in input[i].orEmpty() } }
+    }
+    s.count()
+}
+
+
+fun parse(text: String): Input = text.linesWithoutLastBlanks()
+    .flatMapIndexed { row, s -> s.mapIndexed { col, char -> char.digitToInt() to (row to col) } }
+    .groupBy { it.first }.mapValues { it.value.map { it.second }.toSet() }
+
+val test = """
+    89010123
+    78121874
+    87430965
+    96549874
+    45678903
+    32019012
+    01329801
+    10456732
+""".trimIndent()
 
 fun main() {
     val text = readAllText("local/day10_input.txt")
     val input = parse(text)
-    go() { part1(input) }
-    go() { part2(input) }
-    TODO()
+    go(36) { part1(parse(test)) }
+    go(81) { part2(parse(test)) }
+    go(574) { part1(input) }
+    go(1238) { part2(input) }
     measure(text, parse = ::parse, part1 = ::part1, part2 = ::part2)
 }
 

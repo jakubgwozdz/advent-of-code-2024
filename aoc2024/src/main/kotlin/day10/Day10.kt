64c71ca1d80a57d672f55ff428889eb88df84ed8
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-10 10:02:24 +0100

    day 10

--- aoc2024/src/main/kotlin/day10/Day10.kt
+++ aoc2024/src/main/kotlin/day10/Day10.kt
@@ -12,22 +12,18 @@ enum class Dir { U, R, D, L }
 
 fun parse(text: String): Input = text.linesWithoutLastBlanks()
     .flatMapIndexed { row, s -> s.mapIndexed { col, char -> char.digitToInt() to (row to col) } }
-    .groupBy { it.first }.mapValues { it.value.map { it.second }.toSet() }
+    .groupingBy { it.first }
+    .aggregate { _, acc: MutableSet<Pos>?, (_, pos), _ -> acc?.apply { add(pos) } ?: mutableSetOf(pos) }
 
-fun part1(input: Input): Int = input[0].orEmpty().sumOf { start ->
-    (1..9)
-        .fold(setOf(start)) { acc, i ->
-            acc.flatMap(Pos::adjacents).intersect(input[i].orEmpty())
-        }
-        .count()
-}
+fun part1(input: Input): Int = calculate(input).sumOf { it.size }
 
-fun part2(input: Input) = input[0].orEmpty().sumOf { start ->
-    (1..9)
-        .fold(listOf(start)) { acc, i ->
-            acc.flatMap(Pos::adjacents).filter(input[i].orEmpty()::contains)
-        }
-        .count()
+fun part2(input: Input) = calculate(input).sumOf { it.sum() }
+
+private fun calculate(input: Input) = input[0].orEmpty().map { start ->
+    (1..9).fold(mapOf(start to 1)) { acc, i ->
+        acc.flatMap { (pos, count) -> pos.adjacents().filter(input[i].orEmpty()::contains).map { it to count } }
+            .groupingBy { it.first }.fold(0) { a, (_, e) -> a + e }
+    }.values
 }
 
 fun main() {

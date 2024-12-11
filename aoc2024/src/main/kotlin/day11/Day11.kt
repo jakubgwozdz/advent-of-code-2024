466d4289859ef1c1db317ff9114916682640e11e
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-11 10:28:16 +0100

    day 11 unclean

--- aoc2024/src/main/kotlin/day11/Day11.kt
+++ aoc2024/src/main/kotlin/day11/Day11.kt
@@ -4,17 +4,13 @@ import go
 import measure
 import readAllText
 
-fun Long.blink(): List<Long> {
-    val ts = toString()
-    return when {
-        this == 0L -> listOf(1L)
-        ts.length % 2 == 0 -> listOf(ts.take(ts.length / 2).toLong(), ts.drop(ts.length / 2).toLong())
-        else -> listOf(this * 2024)
-    }
-}
-
 fun List<Pair<Long, Long>>.blink() = flatMap { (s, c) ->
-    s.blink().map { s1 -> s1 to c }
+    val ts = s.toString()
+    when {
+        s == 0L -> listOf(1L to c)
+        ts.length % 2 == 0 -> listOf(ts.take(ts.length / 2).toLong() to c, ts.drop(ts.length / 2).toLong() to c)
+        else -> listOf(s * 2024 to c)
+    }
 }
     .groupingBy { it.first }
     .fold(0L) { a, (_, e) -> a + e }
@@ -24,9 +20,11 @@ typealias Input = List<Long>
 
 fun part1(input: Input) = blinks(input, 25)
 
-private fun blinks(input: Input, number: Int) = (1..number)
-    .fold(input.map { it to 1L }) { acc, step -> acc.blink() }
-    .sumOf { it.second }
+private fun blinks(input: Input, number: Int): Long {
+    var x = input.map { it to 1L }
+    repeat(number) { x = x.blink() }
+    return x.sumOf { it.second }
+}
 
 fun part2(input: Input) = blinks(input, 75)
 

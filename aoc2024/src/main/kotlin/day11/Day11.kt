9e843e0c7c0599638ab48aadd0cc7d928bde0d23
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-11 10:44:44 +0100

    day 11 better

--- aoc2024/src/main/kotlin/day11/Day11.kt
+++ aoc2024/src/main/kotlin/day11/Day11.kt
@@ -4,11 +4,15 @@ import go
 import measure
 import readAllText
 
-fun List<Pair<Long, Long>>.blink() = flatMap { (s, c) ->
-    val ts = s.toString()
-    when {
-        s == 0L -> listOf(1L to c)
-        ts.length % 2 == 0 -> listOf(ts.take(ts.length / 2).toLong() to c, ts.drop(ts.length / 2).toLong() to c)
+fun Input.blink() = flatMap { (s, c) ->
+    when (s) {
+        0L -> listOf(1L to c)
+        in (10..99) -> listOf(s / 10 to c, s % 10 to c)
+        in (1000..9999) -> listOf(s / 100 to c, s % 100 to c)
+        in (100000..999999) -> listOf(s / 1000 to c, s % 1000 to c)
+        in (10000000..99999999) -> listOf(s / 10000 to c, s % 10000 to c)
+        in (1000000000..9999999999) -> listOf(s / 100000 to c, s % 100000 to c)
+        in (100000000000..999999999999) -> listOf(s / 1000000 to c, s % 1000000 to c)
         else -> listOf(s * 2024 to c)
     }
 }
@@ -16,19 +20,19 @@ fun List<Pair<Long, Long>>.blink() = flatMap { (s, c) ->
     .fold(0L) { a, (_, e) -> a + e }
     .toList()
 
-typealias Input = List<Long>
+typealias Input = List<Pair<Long, Long>>
 
 fun part1(input: Input) = blinks(input, 25)
 
 private fun blinks(input: Input, number: Int): Long {
-    var x = input.map { it to 1L }
+    var x = input
     repeat(number) { x = x.blink() }
     return x.sumOf { it.second }
 }
 
 fun part2(input: Input) = blinks(input, 75)
 
-fun parse(text: String) = text.trim().split(" ").map { it.toLong() }
+fun parse(text: String): Input = text.trim().split(" ").map { it.toLong() to 1L }
 
 fun main() {
     val text = readAllText("local/day11_input.txt")

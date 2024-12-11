645770a5e15d50168e650149f8a68f92a9179bbd
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-11 10:22:03 +0100

    day 11 unclean

--- aoc2024/src/main/kotlin/day11/Day11.kt
+++ aoc2024/src/main/kotlin/day11/Day11.kt
@@ -4,69 +4,33 @@ import go
 import measure
 import readAllText
 
-sealed interface Node {
-    fun blink(): Node
-    fun count(): Long
-}
-
-data class Stone(val value: Long) : Node {
-    override fun blink(): Node {
-        return Split(value.blink().map { Stone(it) to 1L })
-    }
-
-    override fun count() = 1L
-}
-
 fun Long.blink(): List<Long> {
     val ts = toString()
-    val next = when {
+    return when {
         this == 0L -> listOf(1L)
-        ts.length % 2 == 0 ->
-            listOf(
-                ts.take(ts.length / 2).toLong(),
-                ts.drop(ts.length / 2).toLong(),
-            )
-
+        ts.length % 2 == 0 -> listOf(ts.take(ts.length / 2).toLong(), ts.drop(ts.length / 2).toLong())
         else -> listOf(this * 2024)
     }
-    return next
 }
 
-fun List<Pair<Node, Long>>.blink() = flatMap { (s, c) ->
-    s.blink().let {
-        when (it) {
-            is Stone -> listOf(it to c)
-            is Split -> it.nodes.map { (s, c1) -> s to c * c1 }
-        }
-    }
+fun List<Pair<Long, Long>>.blink() = flatMap { (s, c) ->
+    s.blink().map { s1 -> s1 to c }
 }
     .groupingBy { it.first }
     .fold(0L) { a, (_, e) -> a + e }
     .toList()
 
-data class Split(val nodes: List<Pair<Node, Long>>) : Node {
-
-    override fun blink() = nodes
-        .blink()
-        .let { Split(it) }
-
-    override fun count() = nodes.sumOf { it.first.count() * it.second }
-}
-
-typealias Input = List<Node>
+typealias Input = List<Long>
 
 fun part1(input: Input) = blinks(input, 25)
 
-private fun blinks(input: Input, number: Int) = input.sumOf {
-    (1..number).fold(it) { acc, step -> acc.blink() }.count()
-}
+private fun blinks(input: Input, number: Int) = (1..number)
+    .fold(input.map { it to 1L }) { acc, step -> acc.blink() }
+    .sumOf { it.second }
 
 fun part2(input: Input) = blinks(input, 75)
 
-
-fun parse(text: String) = text.trim().split(" ").map { Stone(
-    it.toLong()
-) }
+fun parse(text: String) = text.trim().split(" ").map { it.toLong() }
 
 fun main() {
     val text = readAllText("local/day11_input.txt")

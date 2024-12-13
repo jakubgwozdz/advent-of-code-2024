faa8c66642315cbb280361b1a5712c5219e5c784
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-13 08:18:27 +0100

    day 13 part 1

--- aoc2024/src/main/kotlin/day13/Day13.kt
+++ aoc2024/src/main/kotlin/day13/Day13.kt
@@ -9,37 +9,63 @@ typealias Pos = Pair<Long, Long> // x, y
 
 operator fun Pos.plus(other: Pos) = Pos(first + other.first, second + other.second)
 operator fun Pos.times(other: Int) = Pos(first * other, second * other)
+operator fun Pos.times(other: Long) = Pos(first * other, second * other)
 fun Pos.isLessThanOrEqual(other: Pos) = first <= other.first && second <= other.second
 
 data class Case(val a: Pos, val b: Pos, val prize: Pos)
 
 typealias Input = List<Case>
 
-fun price(tA: Int, tB: Int) = tA * 3 + tB
+fun price(tA: Long, tB: Long) = tA * 3 + tB
 
-fun part1(input: Input) = input.sumOf(Case::solve)
+fun part1(input: Input) = input.sumOf { case ->
+    case.solve()
+//        .also { println("$case $it") }
+//        .also {
+//        val solve2 = case.solve2()
+//        if (solve2 != it) {
+//            println("$case solution1 $it solution2 $solve2")
+//            case.solve2()
+//        }
+//    }
+}
 
-fun Case.solve(): Int {
-    var rA = 101
-    var rB = 101
-    var tA = 0
-    while (price(tA, 0) < price(rA, rB) && (a * tA).isLessThanOrEqual(prize)) {
-        var tB = 0
-        while (price(tA, tB) < price(rA, rB) && (a * tA + b * tB).isLessThanOrEqual(prize)) {
-            if (a * tA + b * tB == prize && price(tA, tB) < price(rA, rB)) {
-                rA = tA
-                rB = tB
-            }
-            tB++
+fun Case.solve(): Long {
+    val aStart = 0L
+    val bStart = 0L
+    var ta = aStart
+    var tb = bStart
+    while ((a * (ta) + b * (tb)).isLessThanOrEqual(prize)) {
+        var tb = bStart
+        while ((a * (ta) + b * (tb)).isLessThanOrEqual(prize)) {
+            val pos = a * (ta) + b * (tb)
+            if (pos == prize) return price(ta, tb)
+            tb++
         }
-        tA++
+        ta++
     }
-    return if (rA == 101 || rB == 101) 0 else price(rA, rB)
+    return 0
+}
+
+fun Case.solve2(): Long {
+    val lcm = lcm(a.first, b.first) * lcm(a.second, b.second)
+    val maxOps = lcm
+    val startA = minOf(prize.first / a.first, prize.second / a.second) - lcm
+    val startB = minOf(prize.first / b.first, prize.second / b.second) - lcm
+
+    for (ta in 0..maxOps) {
+        for (tb in 0..maxOps) {
+            val x = a * (startA + ta) + b * (startB + tb)
+            if (x == prize) return price(startA + ta, startB + tb)
+        }
+    }
+
+    return 0
 }
 
 fun part2(input: Input) = input
     .map { it.copy(prize = it.prize.first + 10000000000000 to it.prize.second + 10000000000000) }
-    .sumOf(Case::solve)
+    .sumOf(Case::solve2)
 
 val rA = Regex("""Button A: X\+(.+), Y\+(.+)""")
 val rB = Regex("""Button B: X\+(.+), Y\+(.+)""")
@@ -49,6 +75,11 @@ fun parse(text: String) = text.linesWithoutLastBlanks().chunked(4).map { (sA, sB
     val a = rA.find(sA)!!.destructured.let { (x, y) -> Pos(x.toLong(), y.toLong()) }
     val b = rB.find(sB)!!.destructured.let { (x, y) -> Pos(x.toLong(), y.toLong()) }
     val prize = rP.find(sP)!!.destructured.let { (x, y) -> Pos(x.toLong(), y.toLong()) }
+//    if (gcd(a.first, b.first) != 1L && gcd(a.second, b.second) != 1L) println(
+//        "$a,$b,$prize gcd(X)=${
+//            gcd(a.first, b.first)
+//        }, gcd(Y)=${gcd(a.second, b.second)}"
+//    )
     Case(a, b, prize)
 }
 
@@ -71,12 +102,14 @@ val test = """
 """.trimIndent()
 
 fun main() {
+    go(480) { part1(parse(test)) }
     val text = readAllText("local/day13_input.txt")
     val input = parse(text)
-    go(480) { part1(parse(test)) }
-    go() { part1(input) }
+    go(30973) { part1(input) }
     go() { part2(input) }
     TODO()
     measure(text, parse = ::parse, part1 = ::part1, part2 = ::part2)
 }
 
+tailrec fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
+fun lcm(a: Long, b: Long): Long = a / gcd(a, b) * b

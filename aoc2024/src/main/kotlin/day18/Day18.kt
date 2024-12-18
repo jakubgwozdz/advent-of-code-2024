c7f954310a02912c74cb4623d87142963c746da4
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-18 22:05:48 +0100

    day18 ania algo + union-find + cleanup

--- aoc2024/src/main/kotlin/day18/Day18.kt
+++ aoc2024/src/main/kotlin/day18/Day18.kt
@@ -6,16 +6,17 @@ import measure
 import readAllText
 
 typealias Input = List<Pos>
-typealias Pos = Pair<Int, Int>
 
-enum class Dir { U, R, D, L }
-
-operator fun Pos.plus(d: Dir): Pos = when (d) {
-    Dir.U -> first - 1 to second
-    Dir.R -> first to second + 1
-    Dir.D -> first + 1 to second
-    Dir.L -> first to second - 1
+data class Pos(val row: Int, val col: Int) {
+    override fun toString()= "$row,$col"
 }
+
+fun Pos.neighborsPart1() = sequenceOf(
+    Pos(row - 1, col),
+    Pos(row, col + 1),
+    Pos(row + 1, col),
+    Pos(row, col - 1),
+)
 
 fun part1(input: Input): Int = input.take(1024).toSet().let { fallen ->
 
@@ -27,11 +28,10 @@ fun part1(input: Input): Int = input.take(1024).toSet().let { fallen ->
     while (queue.isNotEmpty()) {
         val (last, count) = queue.removeFirst()
         if (last == end) return count
-        Dir.entries.forEach { dir ->
-            val next = last + dir
+        last.neighborsPart1().forEach { next ->
             if (next !in visited &&
                 next !in fallen &&
-                next.first in 0..end.first && next.second in 0..end.second
+                next.row in 0..end.row && next.col in 0..end.col
             ) {
                 visited += next
                 queue.add(next to count + 1)
@@ -41,26 +41,26 @@ fun part1(input: Input): Int = input.take(1024).toSet().let { fallen ->
     error("No solution found")
 }
 
-fun Pos.neighbors() = listOf(
-    first - 1 to second,
-    first + 1 to second,
-    first to second - 1,
-    first to second + 1,
-    first - 1 to second - 1,
-    first - 1 to second + 1,
-    first + 1 to second - 1,
-    first + 1 to second + 1
+fun Pos.neighborsPart2() = sequenceOf(
+    Pos(row - 1, col),
+    Pos(row - 1, col + 1),
+    Pos(row, col + 1),
+    Pos(row + 1, col + 1),
+    Pos(row + 1, col),
+    Pos(row + 1, col - 1),
+    Pos(row, col - 1),
+    Pos(row - 1, col - 1),
 )
 
 fun part2(input: Input): String {
     val dropped = mutableSetOf<Pos>()
 
     val n = 73 // grid size + borders
-    val uf = UnionFind<Pos>(n*n) { (r, c) -> (r + 1) * n + c + 1 }
+    val uf = UnionFind<Pos>(n * n) { (r, c) -> (r + 1) * n + c + 1 }
 
     fun drop(pos: Pos) {
         dropped += pos
-        pos.neighbors().forEach { if (it in dropped) uf.union(pos, it) }
+        pos.neighborsPart2().forEach { if (it in dropped) uf.union(pos, it) }
     }
 
     val leftBorder = Pos(-1, 1)
@@ -79,7 +79,7 @@ fun part2(input: Input): String {
 
     input.forEach {
         drop(it)
-        if (isBlocked()) return "${it.first},${it.second}"
+        if (isBlocked()) return it.toString()
     }
 
     error("No solution found")
@@ -112,7 +112,7 @@ class UnionFind<T>(size: Int, val indexOp: (T) -> Int) {
 
 fun parse(text: String) = text.linesWithoutLastBlanks().map { line ->
     val (a, b) = line.split(",").map { it.toInt() }
-    a to b
+    Pos(a, b)
 }
 
 fun main() {
@@ -121,27 +121,4 @@ fun main() {
     go(226, "part1(input): ") { part1(input) }
     go("60,46", "part2(input): ") { part2(input) }
     measure(text, parse = ::parse, part1 = ::part1, part2 = ::part2)
-}
-
-class PriorityQueue<E : Any>(val comparator: Comparator<E>, vararg initial: E) {
-
-    private var backingList = mutableListOf<E>().apply { addAll(initial) }
-
-    val size get() = backingList.size
-    fun isNotEmpty(): Boolean = size > 0
-
-    fun poll(): E {
-        check(size > 0)
-        return backingList.removeAt(0)
-    }
-
-    fun offer(e: E) {
-        val index = backingList.binarySearch(e, comparator).let {
-            if (it < 0) -it - 1 else it
-        }
-        backingList.add(index, e)
-    }
-
-    fun toList() = backingList.toList()
-    override fun toString(): String = backingList.toString()
 }

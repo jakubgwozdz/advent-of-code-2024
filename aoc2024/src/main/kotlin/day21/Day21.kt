1526e85bec10b2e045bedb950b2f2fa06132d871
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-21 17:46:03 +0100

    day 21 p1 stupid

--- aoc2024/src/main/kotlin/day21/Day21.kt
+++ aoc2024/src/main/kotlin/day21/Day21.kt
@@ -24,32 +24,44 @@ data class KeyPad(val keys: List<Pair<Pos, Char>>) {
     operator fun get(pos: Pos) = byPos[pos]!!
     operator fun contains(pos: Pos) = pos in byPos
 
-    fun perform(pos: Pos, c: Char): Pair<Pos, Char?> = when (c) {
-        in "v^<>" -> pos + c to null
-        'A' -> pos to this[pos]
-        else -> error("Invalid char: $c")
-    }
-
-    fun shortestPath(start: Pos, end: Pos): List<Char> {
-        val queue = mutableListOf(start to emptyList<Char>())
-        val visited = mutableSetOf(start)
+    fun shortestPaths(startC: Char, endC: Char): Sequence<String> = sequence {
+        val start = get(startC)
+        val end = get(endC)
+        val queue = mutableListOf(Pair(start, ""))
         while (queue.isNotEmpty()) {
             val (pos, path) = queue.removeFirst()
-            if (pos == end) return path
-            for (c in "><^v") {
+            if (pos == end) yield(path + 'A')
+            if (pos.row < end.row) {
+                val c = 'v'
                 val newPos = pos + c
-                if (newPos in this && newPos !in visited) {
-                    visited += newPos
-                    queue += newPos to path + c
-                }
+                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
+            }
+            if (pos.row > end.row) {
+                val c = '^'
+                val newPos = pos + c
+                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
+            }
+            if (pos.col < end.col) {
+                val c = '>'
+                val newPos = pos + c
+                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
+            }
+            if (pos.col > end.col) {
+                val c = '<'
+                val newPos = pos + c
+                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
             }
         }
-        error("No path found")
     }
 
-    fun shortestPath(code: String) =
-        "A$code".zipWithNext().map { (from, to) -> shortestPath(this[from], this[to]) + 'A' }.flatten()
-            .joinToString("")
+    fun minSteps(code: String): Int =
+        "A$code".zipWithNext().sumOf { (from, to) -> shortestPaths(from, to).first().length }
+
+    fun shortestPaths(code: String): List<String> =
+        "A$code".asSequence().zipWithNext().map { (from, to) -> shortestPaths(from, to) }
+            .reduce { acc, list -> acc.flatMap { a -> list.map { b -> a + b } } }
+            .toList()
+
 }
 
 val directional = """
@@ -74,64 +86,19 @@ val numeric = """
 
 data class Robot(val keyPad: KeyPad, val pos: Pos = keyPad['A'])
 
-data class State(
-    val p0presses: String = "",
-    val p1pos: Pos = directional['A'],
-    val p1presses: String = "",
-    val p2pos: Pos = directional['A'],
-    val p2presses: String = "",
-    val p3pos: Pos = numeric['A'],
-    val p3presses: String = "",
-) {
-    fun isLegal() = p1pos in directional && p2pos in directional && p3pos in numeric
-    fun addPress(c: Char): State {
-        val newP0presses = p0presses + c
-        val (newP1pos, newP1press) = directional.perform(p1pos, c)
-        if (newP1press == null) return copy(p0presses = newP0presses, p1pos = newP1pos)
-        val (newP2pos, newP2press) = directional.perform(p2pos, newP1press)
-        if (newP2press == null) return copy(
-            p0presses = newP0presses,
-            p1pos = newP1pos,
-            p1presses = p1presses + newP1press,
-            p2pos = newP2pos
-        )
-        val (newP3pos, newP3press) = numeric.perform(p3pos, newP2press)
-        if (newP3press == null) return copy(
-            p0presses = newP0presses,
-            p1pos = newP1pos,
-            p1presses = p1presses + newP1press,
-            p2pos = newP2pos,
-            p2presses = p2presses + newP2press,
-            p3pos = newP3pos
-        ) else return copy(
-            p0presses = newP0presses,
-            p1pos = newP1pos,
-            p1presses = p1presses + newP1press,
-            p2pos = newP2pos,
-            p2presses = p2presses + newP2press,
-            p3pos = newP3pos,
-            p3presses = p3presses + newP3press
-        )
-    }
-}
+// <v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A
+// <AAv<AA>>^A
 
-fun shortestPath(code: String, robots: List<Robot>): String {
-    println("code: $code")
-    val r0 = numeric.shortestPath(code)
-    println("r0: $r0 (${r0.length})")
-    val r1 = directional.shortestPath(r0)
-    println("r1: $r1 (${r1.length})")
-    val r2 = directional.shortestPath(r1)
-    println("r2: $r2 (${r2.length})")
-    return r2
-}
-//<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-//v<A<AA>^>AvA^<A>vA^Av<<A>^>AvA^Av<<A>^>AAvA<A^>A<A>Av<A<A>^>AAA<A>vA^A
+fun shortestPath(code: String, robots: List<Robot>): Int =
+    numeric.shortestPaths(code)
+        .flatMap { directional.shortestPaths(it) }
+        .minOf { directional.minSteps(it) }
 
 fun part1(input: Input) = input.sumOf {
     val numPart = it.substringBefore('A').toInt()
     val shortest = shortestPath(it, listOf(Robot(numeric), Robot(directional), Robot(directional)))
-    numPart * shortest.length
+    println("$it: $shortest * $numPart")
+    numPart * shortest
 }
 
 fun part2(input: Input) = part1(input).also { TODO() }
@@ -147,10 +114,29 @@ val test = """
 """.trimIndent()
 
 fun main() {
+
+    println("directional:")
+    directional.keys.forEach { (_, from) ->
+        directional.keys.forEach { (_, to) ->
+            println(
+                "$from -> $to: ${
+                    directional.shortestPaths(from, to).toList().map { it to directional.minSteps(it) }
+                }"
+            )
+        }
+    }
+    println()
+    println("numeric:")
+    numeric.keys.forEach { (_, from) ->
+        numeric.keys.forEach { (_, to) ->
+            println("$from -> $to: ${numeric.shortestPaths(from, to).toList().map { it to directional.minSteps(it) }}")
+        }
+    }
+
     go(126384) { part1(parse(test)) }
     val text = readAllText("local/day21_input.txt")
     val input = parse(text)
-    go() { part1(input) }
+    go(157892) { part1(input) }
     go() { part2(input) }
     TODO()
     measure(text, parse = ::parse, part1 = ::part1, part2 = ::part2)

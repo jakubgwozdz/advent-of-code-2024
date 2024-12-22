03e8a2a2b5e87ba5a6529c7302f367cc4e7ec192
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-22 18:01:25 +0100

    day 21 better code

--- aoc2024/src/main/kotlin/day21/Day21.kt
+++ aoc2024/src/main/kotlin/day21/Day21.kt
@@ -7,117 +7,90 @@ import readAllText
 
 typealias Input = List<String>
 
-data class Pos(val row: Int, val col: Int) {
-    operator fun plus(c: Char) = when (c) {
-        '^' -> copy(row = row - 1)
-        'v' -> copy(row = row + 1)
-        '<' -> copy(col = col - 1)
-        '>' -> copy(col = col + 1)
-        else -> error("Invalid direction: $c")
-    }
+//data class Pos(val row: Int, val col: Int) {
+//    operator fun plus(c: Char) = when (c) {
+//        '^' -> copy(row = row - 1)
+//        'v' -> copy(row = row + 1)
+//        '<' -> copy(col = col - 1)
+//        '>' -> copy(col = col + 1)
+//        else -> error("Invalid direction: $c")
+//    }
+//}
+//
+//data class KeyPad(val keys: List<Pair<Pos, Char>>) {
+//    private val byKey = keys.associate { (k, v) -> v to k }
+//    private val byPos = keys.toMap()
+//    operator fun get(c: Char) = byKey[c]!!
+//    operator fun get(pos: Pos) = byPos[pos]!!
+//    operator fun contains(pos: Pos) = pos in byPos
+//
+//    fun shortestCalc(startC: Char, endC: Char): Sequence<String> = sequence {
+//        val start = get(startC)
+//        val end = get(endC)
+//        val queue = mutableListOf(Pair(start, ""))
+//        while (queue.isNotEmpty()) {
+//            val (pos, path) = queue.removeFirst()
+//            if (pos == end) yield(path + 'A')
+//            if (pos.row < end.row) {
+//                val c = 'v'
+//                val newPos = pos + c
+//                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
+//            }
+//            if (pos.row > end.row) {
+//                val c = '^'
+//                val newPos = pos + c
+//                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
+//            }
+//            if (pos.col < end.col) {
+//                val c = '>'
+//                val newPos = pos + c
+//                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
+//            }
+//            if (pos.col > end.col) {
+//                val c = '<'
+//                val newPos = pos + c
+//                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
+//            }
+//        }
+//    }
+//
+//    fun shortestPaths(code: String): Sequence<String> =
+//        "A$code".asSequence().zipWithNext().map { (from, to) -> shortestCalc(from, to) }
+//            .reduce { acc, list -> acc.flatMap { a -> list.map { b -> a + b } } }
+//
+//}
+//
+
+typealias PathMapping = Map<Pair<Char, Char>, String>
+
+data class Path(val steps: String) {
+    fun unroll(mapping: PathMapping) = Unrolled(steps.unrollSteps(mapping))
 }
 
-data class KeyPad(val keys: List<Pair<Pos, Char>>, val steps: Map<Pair<Char, Char>, String>) {
-    private val byKey = keys.associate { (k, v) -> v to k }
-    private val byPos = keys.toMap()
-    operator fun get(c: Char) = byKey[c]!!
-    operator fun get(pos: Pos) = byPos[pos]!!
-    operator fun contains(pos: Pos) = pos in byPos
-
-    fun shortestPreCalc(startC: Char, endC: Char) = steps[startC to endC]!!
-
-    fun shortestCalc(startC: Char, endC: Char): Sequence<String> = sequence {
-        val start = get(startC)
-        val end = get(endC)
-        val queue = mutableListOf(Pair(start, ""))
-        while (queue.isNotEmpty()) {
-            val (pos, path) = queue.removeFirst()
-            if (pos == end) yield(path + 'A')
-            if (pos.row < end.row) {
-                val c = 'v'
-                val newPos = pos + c
-                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
-            }
-            if (pos.row > end.row) {
-                val c = '^'
-                val newPos = pos + c
-                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
-            }
-            if (pos.col < end.col) {
-                val c = '>'
-                val newPos = pos + c
-                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
-            }
-            if (pos.col > end.col) {
-                val c = '<'
-                val newPos = pos + c
-                if (newPos in this@KeyPad) queue += Pair(newPos, path + c)
-            }
+data class Unrolled(val nodes: List<Pair<Path, Long>>) {
+    fun unroll(mapping: PathMapping): Unrolled = buildMap {
+        nodes.forEach { (path, count) ->
+            path.steps.unrollSteps(mapping).forEach { (path, c1) -> this[path] = (this[path] ?: 0L) + c1 * count }
         }
-    }
+    }.let { Unrolled(it.toList()) }
 
-    fun shortestPaths(code: String): Sequence<String> =
-        "A$code".asSequence().zipWithNext().map { (from, to) -> shortestCalc(from, to) }
-            .reduce { acc, list -> acc.flatMap { a -> list.map { b -> a + b } } }
-
-    fun shortestPathSingle(code: String): String =
-        "A$code".asSequence().zipWithNext().map { (from, to) -> shortestPreCalc(from, to) }
-            .reduce { acc, list -> acc + list }
-
+    fun size(): Long = nodes.sumOf { (node, count) -> node.steps.length * count }
 }
 
-sealed interface Node {
-    fun unroll(): Unrolled
-    fun size(): Long
+fun String.unrollSteps(mapping: Map<Pair<Char, Char>, String>) = "A$this".zipWithNext()
+    .groupingBy { it }.eachCount()
+    .map { (fromTo, count) -> Path(mapping[fromTo]!!) to count.toLong() }
+
+fun solve(input: Input, intermediate: Int) = input.sumOf { code ->
+    val numPart = code.substringBefore('A').toInt()
+    val count = generateSequence(Path(code).unroll(numericMapping)) { it.unroll(directionalMapping) }
+        .drop(intermediate)
+        .first().size()
+    numPart * count
 }
 
-data class Path(val steps: String) : Node {
-    override fun unroll(): Unrolled {
-        val nodes = "A$steps".zipWithNext()
-            .groupingBy { it }.eachCount()
-            .map { (fromTo, count) ->
-                val (from, to) = fromTo
-                val path = directional.shortestPreCalc(from, to)
-                Path(path) to count.toLong()
-            }
-        return Unrolled(nodes)
-    }
-
-    override fun size() = steps.length.toLong()
-}
-
-data class Unrolled(val nodes: List<Pair<Path, Long>>) : Node {
-    override fun unroll(): Unrolled {
-        val nodes1 = nodes.map { (node, count) -> node.unroll() to count }
-            .flatMap { (unrolled, count) -> unrolled.nodes.map { it.first to it.second * count } }
-            .groupBy { (path) -> path }
-            .mapValues { (_, list) -> list.sumOf { it.second } }
-            .toList()
-        return Unrolled(nodes1)
-    }
-
-    override fun size(): Long = nodes.sumOf { (node, count) -> node.size() * count }
-}
-
-fun shortestPath(code: String, intermediate: Int): Long {
-    var seq: Node = Path(numeric.shortestPathSingle(code))
-    repeat(intermediate) {
-        seq = seq.unroll()
-    }
-    return seq.size()
-}
-
-fun part1(input: Input) = input.sumOf {
-    val numPart = it.substringBefore('A').toInt()
-    val shortest = shortestPath(it, 2)
-    numPart * shortest
-}
-
-fun part2(input: Input) = input.sumOf {
-    val numPart = it.substringBefore('A').toInt()
-    val shortest = shortestPath(it, 25)
-    numPart * shortest
-}
+fun part1(input: Input) = solve(input, 2)
+fun part2(input: Input) = solve(input, 25)
 
 fun parse(text: String): Input = text.linesWithoutLastBlanks()
 
@@ -135,26 +108,7 @@ fun main() {
 }
 
 
-private fun printCode(keyPad: KeyPad, valName: String, keys: String) {
-    println("val $valName = mapOf(")
-    keys.forEach { from ->
-        keys.forEach { to ->
-            val possible = keyPad.shortestCalc(from, to)
-            val byLen = possible.groupBy {
-                var s = it
-                repeat(5) { s = directional.shortestPathSingle(s) }
-                s.length
-            }
-            val min = byLen.keys.min()
-            val path = byLen[min]!!.single()
-            println("  ('$from' to '$to') to \"$path\",")
-        }
-    }
-    println(")")
-}
-
-
-val directionalSteps = mapOf(
+val directionalMapping = mapOf(
     ('A' to 'A') to "A",
     ('A' to '^') to "<A",
     ('A' to '>') to "vA",
@@ -181,7 +135,7 @@ val directionalSteps = mapOf(
     ('<' to 'v') to ">A",
     ('<' to '<') to "A",
 )
-val numericSteps = mapOf(
+val numericMapping = mapOf(
     ('A' to 'A') to "A",
     ('A' to '0') to "<A",
     ('A' to '1') to "^<<A",
@@ -308,20 +262,24 @@ val numericSteps = mapOf(
 val directional = """
     .^A
     <v>
-""".trimIndent().lines().mapIndexed { row, line ->
-    line.mapIndexedNotNull { col, c ->
-        if (c == '.') null else Pos(row, col) to c
-    }
-}.flatten().let { KeyPad(it, directionalSteps) }
+""".trimIndent()
 
 val numeric = """
     789
     456
     123
     .0A
-""".trimIndent().lines().mapIndexed { row, line ->
-    line.mapIndexedNotNull { col, c ->
-        if (c == '.') null else Pos(row, col) to c
-    }
-}.flatten().let { KeyPad(it, numericSteps) }
+""".trimIndent()
+
+//    .lines().mapIndexed { row, line ->
+//        line.mapIndexedNotNull { col, c ->
+//            if (c == '.') null else Pos(row, col) to c
+//        }
+//    }.flatten().let { KeyPad(it) }
+
+//    .lines().mapIndexed { row, line ->
+//    line.mapIndexedNotNull { col, c ->
+//        if (c == '.') null else Pos(row, col) to c
+//    }
+//}.flatten().let { KeyPad(it) }
 

42154e3f762cf9892d9050564949354eb8e0a35d
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-23 08:13:36 +0100

    day 23 sorted

--- aoc2024/src/main/kotlin/day23/Day23.kt
+++ aoc2024/src/main/kotlin/day23/Day23.kt
@@ -2,31 +2,36 @@ package day23
 
 import go
 import linesWithoutLastBlanks
-import mapParallel
 import measure
 import readAllText
 
 fun part1(input: Input): Int {
     val result = mutableSetOf<Set<String>>()
-    val tComps = input.keys.filter { it[0] == 't' }
-    tComps.forEach { tComp ->
-        input[tComp]!!.forEach { c1 ->
-            input[tComp]!!.forEach { c2 -> if (c1 != c2 && c2 in input[c1]!!) result += setOf(tComp, c1, c2) }
+    input.keys
+        .filter { it[0] == 't' }
+        .forEach { t ->
+            input.keys
+                .filter { a -> a in input[t]!! || t in input[a]!! }
+                .forEach { a ->
+                    input.keys
+                        .filter { b -> b in input[t]!! || t in input[b]!! }
+                        .filter { b -> b in input[a]!! || a in input[b]!! }
+                        .forEach { b -> result += setOf(t, a, b) }
+                }
         }
-    }
     return result.size
 }
 
 typealias Input = Map<String, Set<String>>
 
 fun part2solve(graph: Input): String {
-    val result = mutableSetOf<Set<String>>()
-    graph.keys.forEach { result += setOf(it) }
+    val result = mutableListOf<List<String>>()
+    graph.forEach { result += listOf(it.key) }
     while (true) {
-        val newResult = mutableListOf<Set<String>>()
-        result.forEach { s1 ->
-            graph.filter { (v, es) -> v !in s1 && es.containsAll(s1) }
-                .forEach { newResult += s1 + it.key }
+        val newResult = mutableListOf<List<String>>()
+        result.forEachIndexed { i1, previous ->
+            graph.filter { (v, es) -> v !in previous && previous.all { it in es } }
+                .forEach { newResult += previous + it.key }
         }
         if (newResult.isEmpty()) return result.first().sorted().joinToString(",")
         result.clear()
@@ -34,11 +39,9 @@ fun part2solve(graph: Input): String {
     }
 }
 
-fun part2(graph: Input): String {
-    return graph.toList().mapParallel { (v, es) ->
-        part2solve(graph.filterKeys { it == v || it in es })
-    }.maxBy { it.length }
-}
+fun part2(graph: Input): String = graph.toList()
+    .map { (v, es) -> part2solve(graph.filterKeys { it == v || it in es }) }
+    .maxBy { it.length }
 
 fun parse(text: String): Input = text.linesWithoutLastBlanks()
     .map { it.split("-") }
@@ -46,55 +49,13 @@ fun parse(text: String): Input = text.linesWithoutLastBlanks()
         val v = it.flatMap { (a, b) -> listOf(a, b) }.sorted().distinct()
         val e = it.map { (a, b) -> a to b }.toSet() +
                 it.map { (a, b) -> b to a }.toSet()
-        v.associateWith { v1 -> v.filter { v2 -> (v1 to v2) in e }.toSet() }
+        v.associateWith { v1 -> v.filter { v2 -> v1 < v2 && (v1 to v2) in e }.toSet() }
     }
 
-val example = """
-    kh-tc
-    qp-kh
-    de-cg
-    ka-co
-    yn-aq
-    qp-ub
-    cg-tb
-    vc-aq
-    tb-ka
-    wh-tc
-    yn-cg
-    kh-ub
-    ta-co
-    de-co
-    tc-td
-    tb-wq
-    wh-td
-    ta-ka
-    td-qp
-    aq-cg
-    wq-ub
-    ub-vc
-    de-ta
-    wq-aq
-    wq-vc
-    wh-yn
-    ka-de
-    kh-ta
-    co-tc
-    wh-qp
-    tb-vc
-    td-yn
-    ww-co
-    ww-de
-    ww-ka
-    ww-ta
-""".trimIndent()
-
 fun main() {
-//    go(7) { part1(parse(example)) }
     val text = readAllText("local/day23_input.txt")
     val input = parse(text)
     go(1366) { part1(input) }
-//    go("co,de,ka,ta") { part2(parse(example)) }
-    go("co,de,ka,ta,ww") { part2(parse(example)) }
     go("bs,cf,cn,gb,gk,jf,mp,qk,qo,st,ti,uc,xw") { part2(input) }
     measure(text, parse = ::parse, part1 = ::part1, part2 = ::part2)
 }

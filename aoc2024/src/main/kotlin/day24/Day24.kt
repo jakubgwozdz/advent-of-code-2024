c371187c32236f1c9f3657d988be5ae2ad965881
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-24 08:16:49 +0100

    day 24 part2 yyyyyy not working?

--- aoc2024/src/main/kotlin/day24/Day24.kt
+++ aoc2024/src/main/kotlin/day24/Day24.kt
@@ -3,12 +3,13 @@ package day24
 import go
 import measure
 import readAllText
+import java.time.Instant
 
 data class Instruction(val input1: String, val op: String, val input2: String)
 data class Input(val initial: Map<String, Boolean>, val instructions: Map<String, Instruction>)
 
-fun solve(name: String, results: MutableMap<String, Boolean>, instructions: Map<String, Instruction>): Boolean {
-    return results.getOrPut(name) {
+fun solve(name: String, results: MutableMap<String, Boolean>, instructions: Map<String, Instruction>): Boolean =
+    results.getOrPut(name) {
         val instr = instructions[name]!!
         when (instr.op) {
             "AND" -> solve(instr.input1, results, instructions) and solve(instr.input2, results, instructions)
@@ -17,20 +18,136 @@ fun solve(name: String, results: MutableMap<String, Boolean>, instructions: Map<
             else -> error("Unknown op: ${instr.op}")
         }
     }
-}
 
 //262651624 wrong
 fun part1(input: Input): Long {
     val results = input.initial.toMutableMap()
 
-    return input.instructions.keys.filter { it.startsWith('z') }
-        .sortedDescending()
-        .onEach { solve(it, results, input.instructions) }
-        .fold(0L) { acc, name -> acc * 2 + if (results[name]!!) 1 else 0 }
+    return count('z', results, input.instructions)
 }
 
+private fun count(char: Char, results: MutableMap<String, Boolean>, map: Map<String, Instruction>) =
+    (map.keys + results.keys).filter { it.startsWith(char) }
+        .sortedDescending()
+        .onEach { solve(it, results, map) }
+        .fold(0L) { acc, name -> acc * 2 + if (results[name]!!) 1 else 0 }
+
+fun testAddition(data: Map<String, Boolean>, instructions: Map<String, Instruction>): Long {
+    val results = data.toMutableMap()
+    val x = count('x', results, instructions)
+    val y = count('y', results, instructions)
+    val z = count('z', results, instructions)
+    return z - (x + y)
+}
+
+// cnk,ndq,qwf,z14,z27,z28,z39,z40 wrong???
+
 fun part2(input: Input): Any {
+    println()
+    val results = input.initial.toMutableMap()
+    val pairs = listOf(
+        "z40" to "z39",
+        "z28" to "z27",
+        "cnk" to "qwf",
+        "z14" to "ndq",
+    )
+
+    val instructions = input.instructions.toMutableMap().apply {
+        pairs.forEach { (a, b) -> swap(a, b) }
+    }
+    val x = count('x', results, instructions)
+    val y = count('y', results, instructions)
+    val z = count('z', results, instructions)
+    if (z == x + y) {
+        return pairs.flatMap { it.toList() }.sorted().joinToString(",")
+    }
+
+    println("          " + (45 downTo 0).joinToString("") { "${it / 10}" })
+    println("          " + (45 downTo 0).joinToString("") { "${it % 10}" })
+
+    println("is:       ${z.toString(2)}")
+    println("shouldbe: ${(x + y).toString(2)}")
+
+    // z40: 1->0
+    // z39: 0->1
+    // z28:
+
+    graphviz(input, results, instructions)
+
     TODO()
+}
+
+private fun graphviz(
+    input: Input,
+    results: MutableMap<String, Boolean>,
+    instructions: MutableMap<String, Instruction>
+) {
+    println("digraph G {")
+    input.instructions.toSortedMap().forEach { (k, v) ->
+        val bit = if (solve(k, results, instructions)) "1" else "0"
+        val label = "$k (${v.op} = $bit)"
+        val shape = if (k.startsWith('z')) "box" else "ellipse"
+        println("  $k[label=\"$label\" shape=\"$shape\"];")
+        println("  ${v.input1} -> $k;")
+        println("  ${v.input2} -> $k;")
+    }
+    println("}")
+}
+
+
+fun part2s(input: Input): Any {
+    val keys = input.instructions.keys.sorted()
+    val instructions = input.instructions
+    var instant = Instant.now()
+    keys.indices.forEach { i1 ->
+        (i1 + 1..<keys.size).forEach { i2 ->
+            val s1 = keys[i1]
+            val s2 = keys[i2]
+            val instructions = instructions.toMutableMap().apply { swap(s1, s2) }
+            (i2 + 1..<keys.size).forEach { i1 ->
+                (i1 + 1..<keys.size).forEach { i2 ->
+                    val s3 = keys[i1]
+                    val s4 = keys[i2]
+                    val instructions = instructions.toMutableMap().apply { swap(s3, s4) }
+                    (i2 + 1..<keys.size).forEach { i1 ->
+                        (i1 + 1..<keys.size).forEach { i2 ->
+                            val s5 = keys[i1]
+                            val s6 = keys[i2]
+                            val instructions = instructions.toMutableMap().apply { swap(s5, s6) }
+                            (i2 + 1..<keys.size).forEach { i1 ->
+                                (i1 + 1..<keys.size).forEach { i2 ->
+                                    val s7 = keys[i1]
+                                    val s8 = keys[i2]
+                                    val instructions = instructions.toMutableMap().apply { swap(s7, s8) }
+                                    val result = testAddition(input.initial, instructions)
+                                    if (Instant.now().toEpochMilli() - instant.toEpochMilli() > 1000) {
+                                        println("$s1 $s2 $s3 $s4 $s5 $s6 $s7 $s8 $result")
+                                        instant = Instant.now()
+                                    }
+                                    if (result == 0L) {
+                                        return listOf(s1, s2, s3, s4, s5, s6, s7, s8).sorted().joinToString(",")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    TODO()
+}
+
+private fun <K, V> MutableMap<K, V>.swap(
+    s1: K,
+    s2: K
+): MutableMap<K, V> {
+    val swap = remove(s1)!!
+    this[s1] = this[s2]!!
+    this[s2] = swap
+    return this
 }
 
 val instrRegex = Regex("""(.{3}) (OR|XOR|AND) (.{3}) -> (.{3})""")
@@ -59,13 +176,13 @@ fun main() {
     val input = parse(text)
     go(4) { part1(parse(example1)) }
     go(2024) { part1(parse(example2)) }
-    go(desc = "Part 1:") { part1(input) }
-    go() { part2(input) }
+    go(45213383376616, desc = "Part 1:") { part1(input) }
+    go("", desc = "Part 2:") { part2(input) }
     TODO()
     measure(text, parse = ::parse, part1 = ::part1, part2 = ::part2)
 }
 
-val example1 ="""
+val example1 = """
     x00: 1
     x01: 1
     x02: 1

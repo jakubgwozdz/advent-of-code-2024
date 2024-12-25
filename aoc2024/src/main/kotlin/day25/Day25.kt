990520e326969a0da0c60fa1e85dfe94e06f6721
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-25 08:04:55 +0100

    day 25 o(n^2) but fast

--- aoc2024/src/main/kotlin/day25/Day25.kt
+++ aoc2024/src/main/kotlin/day25/Day25.kt
@@ -1,42 +1,34 @@
 package day25
 
 import go
-import linesWithoutLastBlanks
 import measure
 import readAllText
 
-typealias Input = Pair<List<Int>, List<Int>>
+typealias Input = List<List<String>>
 
-fun part1(input: Input): Int = input.let { (keys, locks) ->
-    var count = 0
-    for (key in keys) {
-        for (lock in locks) {
-            val valid = key <= lock &&
-                    key and 7 <= lock and 7 &&
-                    key and 63 <= lock and 63 &&
-                    key and 511 <= lock and 511 &&
-                    key and 4095 <= lock and 4095
-            if (valid) count++
-        }
-    }
-    count
-}
-
-fun parse(text: String): Input = text.lines().chunked(8).let { chunks ->
+fun part1(input: Input): Int {
     val keys = mutableListOf<Int>()
     val locks = mutableListOf<Int>()
-    chunks.forEach { chunk ->
+    input.forEach { chunk ->
         val isKey = chunk[0] == "....."
         val isLock = chunk[0] == "#####"
         check(isKey xor isLock) { "Invalid chunk: $chunk" }
-        val counts = (0..4).map { col ->
-            chunk.filterNot { it.isBlank() }
-                .count { row -> row[col] == if (isLock) '.' else '#' }
-        }.fold(0) { acc, i -> acc * 8 + i }
-        if (isLock) locks.add(counts) else keys.add(counts)
+        val keyCode = (0..4)
+            .map { col -> chunk.count { row -> row[col] == if (isLock) '.' else '#' } }
+            .fold(0) { acc, i -> acc * 16 + i }
+        if (isLock) locks.add(keyCode) else keys.add(keyCode)
     }
-    keys to locks
+
+    var count = 0
+    val legit = 0b1110111011101110111
+    for (key in keys)
+        for (lock in locks)
+            if ((lock - key) or legit == legit) count++
+    return count
 }
+
+fun parse(text: String): Input = text.lines().chunked(8)
+    .map { it.subList(0, 7) }
 
 val example = """
     #####

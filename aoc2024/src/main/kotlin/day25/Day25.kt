7ed183a86463f9b1990ac278ac6fb1bbf6bc4fd9
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-25 06:47:27 +0100

    day 25 sub optimal

--- aoc2024/src/main/kotlin/day25/Day25.kt
+++ aoc2024/src/main/kotlin/day25/Day25.kt
@@ -5,21 +5,77 @@ import linesWithoutLastBlanks
 import measure
 import readAllText
 
-typealias Input = List<String>
+typealias Input = Pair<List<List<Int>>, List<List<Int>>>
 
-fun part1(input: Input) = input
-    .count()
+fun part1(input: Input): Int = input.let { (keys, locks) ->
+    locks.sumOf { lock ->
+        keys.count { key -> key.zip(lock).all { (k, l) -> k >= l } }
+    }
+}
 
-fun part2(input: Input) = part1(input)
+fun parse(text: String): Input = text.lines().chunked(8).let { chunks ->
+    val keys = mutableListOf<List<Int>>()
+    val locks = mutableListOf<List<Int>>()
+    chunks.forEach { chunk ->
+        val isKey = chunk[0] == "....."
+        val isLock = chunk[0] == "#####"
+        check(isKey xor isLock) { "Invalid chunk: $chunk" }
+        val counts = (0..4).map { col ->
+            chunk.filterNot { it.isBlank() }
+                .count { row -> row[col] == if (isLock) '#' else '.' }
+        }
+        if (isLock) locks.add(counts) else keys.add(counts)
+    }
+    keys to locks
+}
 
-fun parse(text: String) = text.linesWithoutLastBlanks()
+val example = """
+    #####
+    .####
+    .####
+    .####
+    .#.#.
+    .#...
+    .....
+
+    #####
+    ##.##
+    .#.##
+    ...##
+    ...#.
+    ...#.
+    .....
+
+    .....
+    #....
+    #....
+    #...#
+    #.#.#
+    #.###
+    #####
+
+    .....
+    .....
+    #.#..
+    ###..
+    ###.#
+    ###.#
+    #####
+
+    .....
+    .....
+    .....
+    #....
+    #.#..
+    #.#.#
+    #####
+""".trimIndent()
 
 fun main() {
     val text = readAllText("local/day25_input.txt")
     val input = parse(text)
-    go() { part1(input) }
-    go() { part2(input) }
-    TODO()
-    measure(text, parse = ::parse, part1 = ::part1, part2 = ::part2)
+    go(3) { part1(parse(example)) }
+    go(3338) { part1(input) }
+    measure(text, parse = ::parse, part1 = ::part1, part2 = ::part1)
 }
 

2c07fc88e361d00704dabbfb83a072f8fb431cf5
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2024-12-25 07:01:56 +0100

    day 25 sub optimal 2

--- aoc2024/src/main/kotlin/day25/Day25.kt
+++ aoc2024/src/main/kotlin/day25/Day25.kt
@@ -5,25 +5,34 @@ import linesWithoutLastBlanks
 import measure
 import readAllText
 
-typealias Input = Pair<List<List<Int>>, List<List<Int>>>
+typealias Input = Pair<List<Int>, List<Int>>
 
 fun part1(input: Input): Int = input.let { (keys, locks) ->
-    locks.sumOf { lock ->
-        keys.count { key -> key.zip(lock).all { (k, l) -> k >= l } }
+    var count = 0
+    for (key in keys) {
+        for (lock in locks) {
+            val valid = key <= lock &&
+                    key and 7 <= lock and 7 &&
+                    key and 63 <= lock and 63 &&
+                    key and 511 <= lock and 511 &&
+                    key and 4095 <= lock and 4095
+            if (valid) count++
+        }
     }
+    count
 }
 
 fun parse(text: String): Input = text.lines().chunked(8).let { chunks ->
-    val keys = mutableListOf<List<Int>>()
-    val locks = mutableListOf<List<Int>>()
+    val keys = mutableListOf<Int>()
+    val locks = mutableListOf<Int>()
     chunks.forEach { chunk ->
         val isKey = chunk[0] == "....."
         val isLock = chunk[0] == "#####"
         check(isKey xor isLock) { "Invalid chunk: $chunk" }
         val counts = (0..4).map { col ->
             chunk.filterNot { it.isBlank() }
-                .count { row -> row[col] == if (isLock) '#' else '.' }
-        }
+                .count { row -> row[col] == if (isLock) '.' else '#' }
+        }.fold(0) { acc, i -> acc * 8 + i }
         if (isLock) locks.add(counts) else keys.add(counts)
     }
     keys to locks
